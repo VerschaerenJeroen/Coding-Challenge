1f81d2869f6d3f2b8031c67ace3af8a2c564096f
VerschaerenJeroen <verschaerenjeroen@gmail.com>
2023-02-11 12:32:31 +0100

    Refactor

--- PhoneNumberSum.js
+++ PhoneNumberSum.js
@@ -1,19 +1,17 @@
 const phoneNumber = 492531984;
-let tempArray = [];
 
 console.log(sum(phoneNumber));
 
 function sum(number) {
+    let tempArray = [];
     let numberArray = number.toString().match(/\d{1,2}/g)
     for (let index = 0; index < numberArray.length; index++) {
         let elementSum = numberArray[index].split('').map(Number).reduce(function (a, b) { return a + b; }, 0);
         tempArray.push(elementSum);
     }
     console.log(tempArray)
-    output = tempArray;
-    tempArray = [];
     if (numberArray.length > 1) {
-        sum(output.join(''));
+        sum(tempArray.join(''));
     }
-    return output.join('');
+    return tempArray.join('');
 }
