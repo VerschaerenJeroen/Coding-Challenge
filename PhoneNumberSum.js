35ab721b03a575d47e44b8c774da9279824ac7cc
VerschaerenJeroen <verschaerenjeroen@gmail.com>
2023-02-11 12:49:05 +0100

    Fixed small bug

--- PhoneNumberSum.js
+++ PhoneNumberSum.js
@@ -3,15 +3,16 @@ const phoneNumber = 492531984;
 console.log(sum(phoneNumber));
 
 function sum(number) {
-    let tempArray = [];
+    tempArray = [];
     let numberArray = number.toString().match(/\d{1,2}/g)
     for (let index = 0; index < numberArray.length; index++) {
         let elementSum = numberArray[index].split('').map(Number).reduce(function (a, b) { return a + b; }, 0);
         tempArray.push(elementSum);
     }
     console.log(tempArray)
+    tempArray;
     if (numberArray.length > 1) {
         sum(tempArray.join(''));
     }
-    return tempArray.join('');
+    return tempArray.toString().split('').map(Number).reduce(function (a, b) { return a + b; }, 0);
 }
